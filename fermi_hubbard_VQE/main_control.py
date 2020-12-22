ecad7aa9f11e4369472a71db03f1d6af604848d6
dh-dz <65077240+dh-dz@users.noreply.github.com>
2020-12-22 13:20:58 -0600

    added simplified version

--- fermi_hubbard_VQE/main_control.py
+++ fermi_hubbard_VQE/main_control.py
@@ -1,13 +1,13 @@
-from More_entanglement import fermi_hubbard_half_filling_more_entanglement
+from fermi_hubbard_VQE import fh_VQE
 import numpy as np
-(E1, filling_gs, sweet_spot) = fermi_hubbard_half_filling_more_entanglement(1, 8, 0, 2, 0, 100, 20, False, 1)
+(E1, filling_gs, sweet_spot) = fh_VQE(1, 8, 0, 2, 0, 100, 20, False, 1)
 print(E1, filling_gs)
 E0 = E1 - 1
 iter = 1
 while abs(E0-E1) > 1e-6:
     E0 = E1
     params_better = sweet_spot
-    (E1, filling_gs, sweet_spot) = fermi_hubbard_half_filling_more_entanglement(1, 8, 0, 2, 0, 100, 20, True, params_better)
+    (E1, filling_gs, sweet_spot) = fh_VQE(1, 8, 0, 2, 0, 100, 20, True, params_better)
     iter = iter + 1
     print(E1, filling_gs)
 print(sweet_spot)
